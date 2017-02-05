cb192b391b8b2993b4676b380333150367bafdf6
Andres Guerra <andreu.g.95@gmail.com>
2017-02-05 03:49:51 -0500

    Prueba Conexión Base de Datos

--- api/controllers/BusquedaHabitacionController.js
+++ api/controllers/BusquedaHabitacionController.js
@@ -6,6 +6,14 @@
  */
 
 module.exports = {
-	
+  register: function (req, res) {
+      //console.log(req.param('roomType'));
+      
+      Habitacion.find({
+          precio: { '>=': req.param('minPrice')},
+          precio: { '<=': req.param('maxPrice')},
+          tipo: req.param('roomType')          
+      }).exec(console.log);
+  }
 };
 
