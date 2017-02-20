3135a930bdde457163c84c30da0a9acb9efafc45
fabriram20 <fabriram20@gmail.com>
2017-02-20 02:08:18 -0500

    mostrar reserva, corregir!!

--- api/controllers/ClienteController.js
+++ api/controllers/ClienteController.js
@@ -6,55 +6,7 @@
  */
 
 module.exports = {
-    buscarCedula: function (req, res) {
-        //        if (req.method == 'POST') {
-        //            //console.log(req.param('id_hab'));
-        //            Cliente.find({
-        //                cedula: req.param('cedula')
-        //            }).exec(function (err, habitaciones) {
-        //                if (err) {
-        //                    return res.serverError(err);
-        //                }
-        //
-        ////                return res.view('consulta', {
-        ////                    pclassR: 'active',
-        ////                    pclassC: 'disabled',
-        ////                    tclassR: 'in active',
-        ////                    tclassC: ' ',
-        //////                    habitacionesF: habitaciones,
-        //////                    nombres: req.param('nombres'),
-        //////                    apellidos: req.param('apellidos'),
-        //////                    cedula: req.param('cedula'),
-        //////                    email: req.param('email'),
-        //////                    direccion: req.param('direccion'),
-        //////                    telefono: req.param('telefono'),
-        //////                    fecha_reserva: req.param('fecha_reserva'),
-        //////                    dias: req.param('dias'),
-        //////                    num_tarjeta: req.param('num_tarjeta'),
-        //////                    personas: req.param('personas')
-        ////                });
-        //            });
-        //        }
-        return res.view('consulta', {
-            pclassR: 'active',
-            pclassC: 'disabled',
-            tclassR: 'in active',
-            tclassC: ' ',
-            //                    habitacionesF: habitaciones,
-            //                    nombres: req.param('nombres'),
-            //                    apellidos: req.param('apellidos'),
-            //                    cedula: req.param('cedula'),
-            //                    email: req.param('email'),
-            //                    direccion: req.param('direccion'),
-            //                    telefono: req.param('telefono'),
-            //                    fecha_reserva: req.param('fecha_reserva'),
-            //                    dias: req.param('dias'),
-            //                    num_tarjeta: req.param('num_tarjeta'),
-            //                    personas: req.param('personas')
-        });
-    },
-
-
+   
     buscar: function (req, res) {
         //console.log(req.param('roomType'));
         if (req.method == 'POST') {
@@ -63,7 +15,7 @@ module.exports = {
 
             sql = "select * from cliente where cedula = '" + params.cedula+"'";
 
-            Cliente.query(sql, [], function (err, cliente) {
+            Cliente.query(sql, [], function (err, clientes) {
                 if (err) {
                     return res.serverError(err);
                 }
@@ -71,16 +23,15 @@ module.exports = {
                 /*return res.view('rooms', {
                     habitaciones: habitaciones
                 });*/
-                
-                sails.log.info(cliente);
+                                
+                sails.log.info(clientes);
 
                 return res.view('consulta', {
                     pclassR: 'active',
                     pclassC: 'disabled',
                     tclassR: 'in active',
                     tclassC: ' ',
-                    cliente: cliente,
-                    nombre: cliente.nombre                    
+                    clientes: clientes                   
                 });
             });
         }
